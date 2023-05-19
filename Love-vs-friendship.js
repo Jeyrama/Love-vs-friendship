177b08b948f7c1ec8dbce613d96e765986fbf5d0
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-05-19 11:46:11 -0700

    Update Love-vs-friendship.js

--- Love-vs-friendship.js
+++ Love-vs-friendship.js
@@ -19,3 +19,14 @@ function wordsToMarks(str) {
     sum += str.charCodeAt(i) - 96;
   return sum;
 }
+
+// or
+
+function wordsToMarks(string) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let marks = 0;
+  string.split('').forEach((char) => {
+    marks += alphabet.indexOf(char) + 1
+  });
+  return marks;
+}
